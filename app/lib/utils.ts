1f26f4d816cbeceead4c3cb2bed30c1c2dc7159d
jin <crispy4343@gmail.com>
2025-12-16 13:03:44 +0900

    feat: shadcn baseUI 버전 세팅

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,5 +1,11 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
 import type { ToJson } from '~/common/types';
 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
 // * localize 템플릿 문자열 치환
 export const replaceT = (template: string, params: Record<string, string>) => {
   return template.replace(/{{(.*?)}}/g, (_, key) => params[key.trim()] ?? key);
